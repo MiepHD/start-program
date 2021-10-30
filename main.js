0fb257697acc7edd742e286ce2f91f657f2c48f2
MiepHD <63968466+MiepHD@users.noreply.github.com>
2021-10-30 16:18:43 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,95 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
-
-const {app, BrowserWindow} = electron;
+const { ipcRenderer } = require('electron');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('data.sqlite3');
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
+let addWindow;
 
-// Listen for app to be ready
+// Beim Starten ausf�hren
 app.on('ready', function(){
-	//Create window
-	mainWindow = new BrowserWindow({});
-	//Load html
+	//Hauptfenster erstellen
+	console.log("Erstelle Hauptfenster")
+	mainWindow = new BrowserWindow({
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false
+		}
+	});
+	//HTML laden
+	console.log("Lade HTML")
 	mainWindow.loadURL(url.format({
-		pathname: path.join(__dirname, 'mainWindow.html'),
+		pathname: path.join(__dirname, 'main.html'),
 		protocol: 'file:',
 		slashes: true
 	}));
-
+	//Schlie�en
+	mainWindow.on('closed', function(){
+		app.quit();
+	});
+	//Men�
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+	Menu.setApplicationMenu(mainMenu);
 });
+
+//Men� erstellen
+const mainMenuTemplate = [
+	{
+		label:'Objekte',
+		submenu:[
+			{
+				label: 'Objekt hinzuf�gen',
+				accelerator: process.platform == 'darwin' ? 'Command+N' :
+				'Ctrl+N',
+				click(){
+					mainWindow.send('item:add');
+					console.log('Objekt hinzufügen');
+				}
+			},
+			{
+				label: 'Alle Objekte l�schen',
+				click(){
+					mainWindow.send('item:clear');
+				}
+			},
+			{
+				label: 'Verlassen',
+				accelerator: process.platform == 'darwin' ? 'Command+Q' :
+				'Ctrl+Q',
+				click(){
+					app.quit();
+					console.log('Anwendung geschlossen')
+				}
+			}
+		]
+	},
+	{
+		label:'Werkzeuge',
+		submenu:[
+			{
+				label: "Entwicklertools",
+				accelerator: process.platform == 'darwin' ? 'Command+I' :
+				'Ctrl+I',
+				click(itme, focusedWindow){
+					focusedWindow.toggleDevTools();
+				}
+			},
+			{
+				label: "Hauptseite",
+				accelerator: process.platform == 'darwin' ? 'Command+H' :
+				'Ctrl+H',
+				click(){
+					mainWindow.send('item:home')
+				}
+			}
+		]
+	}
+];
+
+//fix f�r Mac
+if(process.platform == 'darwin'){
+	mainMenuTemplate.unshift({});
+};
